0e6125883727073a34043f45235512aa4d9ee68f
kyon <neon9109@gmail.com>
2015-04-10 10:13:37 +0800

    initial

---
+++ src/com/kyon/levelup/krnote/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kyon.levelup.krnote.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kyon.levelup.krnote.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
